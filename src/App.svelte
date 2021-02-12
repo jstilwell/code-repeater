8bc2b8fd64aa9dff674eb77d2cebc88082f01c58
Jesse Stilwell <jesse.stilwell@gmail.com>
2021-02-12 17:50:26 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+	import CodeHandler from './CodeHandler.svelte'
+
+	export let siteUrl;
+</script>
+
+<header>
+	<CodeHandler siteUrl={siteUrl} />
+</header>
+
+<main>
+	<article>
+		<h5>HOW DO YOU USE THIS?</h5>
+		<p>Place your code in the input field and click <code>SET IT!</code></p>
+		<p>You can also add <code>?code=</code> to the URL followed by the text or code that you want to repeat. This is
+			good for sharing them with others.</p>
+		<p>Click the <code>COPY URL</code> button to share the link with friends.</p>
+		<h5>EXAMPLE:</h5>
+		<code>{siteUrl}?code=rxfc2m23</code>
+
+	</article>
+</main>
+
+<footer>
+	<p>Created by <a href="https://stilwell.io" target="_blank">Jesse Stilwell</a>. View source on <a
+			href="https://github.com/jstilwell/bigscreen-room-code-speaker" target="_blank">Github</a>.
+	</p>
+</footer>
---
+++ src/CodeHandler.svelte
@@ -0,0 +1,98 @@
+<script>
+    import CopyToClipboard from "svelte-copy-to-clipboard";
+    export let siteUrl;
+
+    const handleSuccessfullyCopied = (e) => {
+        console.log(`Successfully copied to clipboard! ${e}`);
+    }
+
+    const handleFailedCopy = () => {
+        console.log('Failed to copy :(');
+    }
+
+    let theCode;
+    let buildCode;
+    let slowCode;
+    let fullUrl;
+    let codeStatus = { isPlaying: false }
+
+    // Get code from URL
+    let arr = document.URL.match(/code=([a-zA-Z0-9_.-]+)/)
+
+    // Check to see if parameter is passed in URL
+    if (arr) {
+        theCode = arr[1]
+    } else {
+        theCode = 'NONE'
+    }
+
+    // Instantiate text-to-speech object
+    let utter = new SpeechSynthesisUtterance()
+
+    function playCode() {
+
+        // Add spaces to string to break it up for character-by-character playback
+        slowCode = theCode.replace(/(.{1})/g, "$1 ")
+        fullUrl = `${siteUrl}code=?${theCode}`
+
+        utter.lang = "en-US"
+        utter.text = `The code is ${slowCode}`
+        utter.volume = 0.7
+        utter.rate = 0.25
+
+        window.speechSynthesis.speak(utter)
+
+        // If codeStatus.isPlaying is toggled to true by clicking the button, 
+        // continue loop until button is clicked again.
+        setTimeout(function () {
+            if (codeStatus.isPlaying) {
+                playCode()
+            } else {
+                stopPlaying()
+            }
+        }, 500)
+    }
+
+    // Toggle loop status between true and false.
+    // If loop is playing and hasn't been toggled by the button, continue playing the loop.
+    function togglePlay() {
+        codeStatus.isPlaying = !codeStatus.isPlaying
+        if (codeStatus.isPlaying) {
+            playCode()
+        }
+    }
+
+    function stopPlaying() {
+        window.speechSynthesis.cancel()
+    }
+
+    // Handle input from form if no code is specified in URL
+    function setCode(input) {
+        theCode = input
+        fullUrl = `${siteUrl}code=?${theCode}`
+    }
+
+    // Reset the code to NONE and toggle isPlaying off if needed
+    function resetCode() {
+        if (codeStatus.isPlaying) {
+            codeStatus.isPlaying = !codeStatus.isPlaying
+        }
+        theCode = 'NONE'
+    }
+</script>
+
+{#if theCode === 'NONE'}
+<center><input bind:value={buildCode} placeholder="Enter code to repeat"></center>
+<button on:click={setCode(buildCode)}>SET IT!</button>
+{:else}
+<h1>{theCode}</h1>
+{#if !codeStatus.isPlaying}
+<button on:click={togglePlay}><strong>PLAY IT!</strong></button>
+{:else}
+<button on:click={togglePlay}><strong>STOP PLAYING IT!</strong></button>
+{/if}
+<CopyToClipboard text={fullUrl} on:copy={handleSuccessfullyCopied} on:fail={handleFailedCopy} let:copy>
+    <button on:click={copy}>COPY URL</button>
+</CopyToClipboard>
+<button on:click={resetCode}>RESET</button>
+{/if}
